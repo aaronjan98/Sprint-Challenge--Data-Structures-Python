638333bca98f21ebbcfe1e9f80ee236e550ba32d
aaronjan <aaronjan98@gmail.com>
2020-05-15 09:15:07 -0700

    passed test for ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.buffer_list = []
+        self.counter = 0
 
     def append(self, item):
-        pass
+
+        if len(self.buffer_list) < self.capacity:
+            self.counter += 1
+            self.buffer_list.append(item)
+        else:
+            if self.counter is self.capacity:
+                self.counter = 0
+            print('self.counter:', self.counter)
+            self.buffer_list.pop(self.counter)
+            self.counter += 1
+            self.buffer_list.insert(self.counter-1, item)
 
     def get(self):
-        pass
+        print(self.buffer_list)
+        return self.buffer_list
